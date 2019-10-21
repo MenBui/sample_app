9a9bf5f5bd8c56c91b5d34be72663e34105ce61a
Men Bui <menbui120198@gmail.com>
2019-10-21 13:24:08 +0700

    gichapter6: Modelling users

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = Settings.valid_email_regex
+
+  before_save{email.downcase!}
+
+  validates :name, presence: true, length: {maximum: Settings.maximum_name}
+  validates :email, presence: true, length: {maximum: Settings.maximum_email},
+   format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :password, presence: true, length: {minimum: Settings.maximum_pw}
+
+  has_secure_password
+end
