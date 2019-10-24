c6c48579abc7425458fd9d56598886644f844dc0
Men Bui <menbui120198@gmail.com>
2019-10-24 15:45:22 +0700

    gitchapter_9: Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.valid_email_regex
+  attr_accessor :remember_token
 
   before_save{email.downcase!}
 
@@ -7,6 +8,36 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: Settings.maximum_email},
    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
   validates :password, presence: true, length: {minimum: Settings.maximum_pw}
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update remember_digest: nil
+  end
 
   has_secure_password
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,10 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker")
 
-const webpacker = require("webpacker")
-environment.plugins.prepend("Provide"),
-  new webpacker.ProvidePlugin({
+const webpack = require("webpack")
+environment.plugins.prepend("Provide",
+  new webpack.ProvidePlugin({
     $: "jquery/src/jquery",
     jQuery: "jquery/src/jquery"
   })
-
+)
 module.exports = environment
