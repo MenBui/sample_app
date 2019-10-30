4796650a166a494b57a8285cc1616b1be99fc197
Men Bui <menbui120198@gmail.com>
2019-10-30 08:50:30 +0700

    chapter_11: Account activation

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,24 @@
+class AccountActivationsController < ApplicationController
+  before_action :find_user, only: :edit
+
+  def edit
+    if !user.activated? && user&.authenticated?(:activation, params[:id])
+      user.activate
+      log_in @user
+      flash[:success] = t "account_activated"
+      redirect_to @user
+    else
+      flash[:danger] = t "invalid_activation_link"
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def find_user
+    return if @user = User.find_by(id: params[:id])
+
+    flash[:danger] = t "not_found"
+    redirect_to root_url
+  end
+end
