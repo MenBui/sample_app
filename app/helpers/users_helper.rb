c6cf6cc0b4e5b7b2b49974156112542afd024b55
Men Bui <menbui120198@gmail.com>
2019-11-07 09:37:20 +0700

    chapter_14: Following users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,15 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag gravatar_url, alt: user.name, class: "gravatar"
   end
+
+  def find_active_relationships
+    return if current_user.active_relationships.find_by(followed_id: @user.id)
+
+    flash[:danger] = t "not_foud"
+    redirect_to root_url
+  end
+
+  def active_relationships_build
+    current_user.active_relationships.build
+  end
 end
--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,6 +1,8 @@
-$('#micropost_image').bind('change', function () {
-  var sizeInMegabytes = this.files[0].size/1024/1024;
-  if (size_in_megabytes > 5) {
-    alert(I18n.t 'maximun_file');
-  }
-});
+$(document).ready(function() {
+  $('#micropost_image').bind('change', function () {
+    var sizeInMegabytes = this.files[0].size/1024/1024;
+    if (sizeInMegabytes > 5) {
+      alert(I18n.t('maximun_file'));
+    }
+  });
+})
